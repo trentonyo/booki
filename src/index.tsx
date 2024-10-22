b1203de2aa42ee413eef6390d1647dc40c2a61a5
TrentonYo <trentonyo@gmail.com>
2024-10-22 13:45:52 -0700

    Refactor index.tsx to use App component
    Replaced the HomeServerComponent with a centralized App component import to simplify the main entry point and improve maintainability. This change helps in better organizing the application structure.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HomeServerComponent from "./components/server/homeServerComponent";
-
-const App = () => (
-    <HomeServerComponent />
-);
+import App from "./app";
 
 ReactDOM.render(<App />, document.getElementById('root'));
