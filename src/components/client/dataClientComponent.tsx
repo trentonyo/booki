f5c84ab5c34456337323bd9f049c5e7d2a7d19f0
TrentonYo <trentonyo@gmail.com>
2024-12-08 13:54:04 -0800

    Add DataSetFrame and DatumFrame components to display data
    Introduce the DataSetFrame and DatumFrame components for rendering fetched data in a structured format with image and JSON display. Update DataClientComponent to use these components, enhancing readability and maintainability. Add new CSS classes to support height adjustments necessary for the updated UI layout.

--- src/components/client/dataClientComponent.tsx
+++ src/components/client/dataClientComponent.tsx
@@ -1,4 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
+
+interface DataSetFrameProps {
+    rawData: string;
+}
+
+const DataSetFrame: React.FC<DataSetFrameProps> = ({rawData}) => {
+    const dataLines = rawData.split("\n");
+
+    return (
+        <div className="flex flex-col gap-4">
+            {dataLines.map((line, index) => {return <div key={index}><DatumFrame datum={line} /></div>})}
+        </div>
+    );
+}
+
+interface DatumFrameProps {
+    datum: string;
+}
+
+const DatumFrame: React.FC<DatumFrameProps> = ({datum}) => {
+    let [imageURI, gameState] = datum.split("\\", 2)
+    imageURI = `api/data/${imageURI}.png`
+    gameState = gameState ? JSON.parse(decodeURIComponent(gameState)) : undefined
+    console.log(gameState)
+
+    return gameState === undefined ? null : (
+        <div className="flex flex-col gap-4 h-full">
+            <img src={imageURI} alt={imageURI} className="h-2/3"/>
+            <textarea readOnly={true} value={JSON.stringify(gameState, null, 2)} />
+        </div>
+    )
+}
 
 const DataClientComponent: React.FC = () => {
     const [dataSetName, setDataSetName] = useState<string | null>(null);
@@ -36,7 +68,7 @@ const DataClientComponent: React.FC = () => {
         <div>
             <h1>Data Validation</h1>
             {fetchedData ? <div>Data fetched successfully.</div> : <div>Loading data...</div>}
-            {fetchedData ? <div>{fetchedData}</div> : <div>No data found.</div>}
+            {fetchedData ? <DataSetFrame rawData={fetchedData}/> : <div>No data found.</div>}
         </div>
     );
 };
